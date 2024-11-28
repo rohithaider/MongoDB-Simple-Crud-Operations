095382e47576c7651145a1950594d76aacc63f19
mohtashim haider <mohtashimhaider@mohtashims-MacBook-Air.local>
2024-11-28 13:22:20 +0600

    running from mac

--- simple-crud-server/index.js
+++ simple-crud-server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-const port  = process.env.PORT || 5000;
+const port  = process.env.PORT || 5001;
 const app = express();
 // rohithaider
 // WkyGdwMdI1nbXhQs
